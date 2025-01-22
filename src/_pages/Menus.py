117ea57d814b66d1cf49b8c3db6b65beb8b50659
Arthur Antero <arthurantero@hotmail.com>
2025-01-22 13:08:21 -0300

    ajeitando problema de exibicao de imagem

--- src/_pages/Menus.py
+++ src/_pages/Menus.py
@@ -30,9 +30,11 @@ def list_menus_page():
 
                 if menu_photo:
                     try:
-                        menu_photo_bytes = bytes.fromhex(menu['menu_photo'][2:])
+                        # Convert memoryview to bytes
+                        menu_photo_bytes = bytes(menu_photo)
                         image = Image.open(io.BytesIO(menu_photo_bytes))
 
+                        # Resize for display
                         max_width = 300
                         image.thumbnail((max_width, max_width))
 

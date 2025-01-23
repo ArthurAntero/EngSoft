5c22dd9ea80e3b71b4a7dcbdcadda148aa4d769e
Arthur de Sá Antero <88858866+ArthurAntero@users.noreply.github.com>
2025-01-22 21:15:14 -0300

    Update RestaurantCreate.py

--- src/_pages/RestaurantCreate.py
+++ src/_pages/RestaurantCreate.py
@@ -1,35 +1,34 @@
-import time
 import streamlit as st
 from api.restaurants import Restaurant
 from globals import logged_user
 
 def create_restaurant_page():
     if logged_user.get("id") is not None:
-        st.header("Criar um Novo Restaurante")
+        st.header("Create a New Restaurant")
 
-        name = st.text_input("Nome do Restaurante")
-        location = st.text_input("Localização")
-        description = st.text_area("Descrição")
-        category = st.text_input("Categoria")
+        name = st.text_input("Restaurant Name")
+        location = st.text_input("Location")
+        description = st.text_area("Description")
+        category = st.text_input("Category")
 
-        if st.button("Criar Restaurante"):
+        if st.button("Create Restaurant"):
             restaurant_instance = Restaurant(
                 name=name, 
                 location=location, 
                 description=description, 
                 category=category, 
+                
                 user_id=logged_user["id"]
             )
             
             if restaurant_instance.create_restaurant(logged_user["id"]):
-                st.success("Restaurante criado com sucesso!")
-                time.sleep(2)
+                st.success("Restaurant created successfully!")
                 st.experimental_set_query_params(page="Restaurants")
                 st.rerun()
             else:
-                st.error("Ocorreu um erro ao criar o restaurante. Por favor, tente novamente.")
+                st.error("There was an error creating the restaurant. Please try again.")
             st.rerun()
     else:
-        st.warning("Você precisa fazer login para criar um restaurante.")
+        st.warning("You need to log in to create a restaurant.")
         st.experimental_set_query_params(page="login")
         st.rerun()

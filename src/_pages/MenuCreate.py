2774fa229f1fede52ab14a0e00eb8fa0f193bdae
Arthur de Sá Antero <88858866+ArthurAntero@users.noreply.github.com>
2025-01-22 21:13:55 -0300

    Update MenuCreate.py

--- src/_pages/MenuCreate.py
+++ src/_pages/MenuCreate.py
@@ -1,4 +1,3 @@
-import time
 import streamlit as st
 from api.menus import Menu
 from api.restaurants import Restaurant
@@ -6,23 +5,23 @@ from globals import logged_user
 
 def create_menu_page():
     if logged_user.get("id") is not None:
-        st.header("Criar um Novo Menu")
+        st.header("Create a New Menu")
 
         restaurant_model = Restaurant()
         user_restaurants = restaurant_model.fetch_restaurants_by_user(logged_user["id"])
 
         if not user_restaurants:
-            st.info("Você precisa criar um restaurante antes de adicionar um menu.")
+            st.info("You need to create a restaurant before adding a menu.")
             return
 
         restaurant_options = {restaurant["name"]: restaurant["id"] for restaurant in user_restaurants}
-        selected_restaurant = st.selectbox("Selecione um Restaurante", list(restaurant_options.keys()))
+        selected_restaurant = st.selectbox("Select a Restaurant", list(restaurant_options.keys()))
 
-        name = st.text_input("Nome do Menu")
-        description = st.text_area("Descrição")
-        menu_photo = st.file_uploader("Carregar uma Foto do Menu", type=["jpg", "jpeg", "png"])
+        name = st.text_input("Menu Name")
+        description = st.text_area("Description")
+        menu_photo = st.file_uploader("Upload a Menu Photo", type=["jpg", "jpeg", "png"])
 
-        if st.button("Criar Menu"):
+        if st.button("Create Menu"):
             if selected_restaurant and name and description:
                 photo_bytes = None
                 if menu_photo:
@@ -37,15 +36,14 @@ def create_menu_page():
                 )
 
                 if menu_instance.create_menu():
-                    st.success("Menu criado com sucesso!")
-                    time.sleep(2)
+                    st.success("Menu created successfully!")
                     st.experimental_set_query_params(page="Menus")
                     st.rerun()
                 else:
-                    st.error("Ocorreu um erro ao criar o cardápio. Por favor, tente novamente.")
+                    st.error("There was an error creating the menu. Please try again.")
             else:
-                st.error("Por favor, preencha todos os campos obrigatórios.")
+                st.error("Please fill in all required fields.")
     else:
-        st.warning("Você precisa fazer login para criar um menu.")
+        st.warning("You need to log in to create a menu.")
         st.experimental_set_query_params(page="login")
         st.rerun()

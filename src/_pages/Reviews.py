3addb82ac2069cf3315a45b6d31e539825cbbbf0
Arthur de Sá Antero <88858866+ArthurAntero@users.noreply.github.com>
2025-01-22 21:16:17 -0300

    Update Reviews.py

--- src/_pages/Reviews.py
+++ src/_pages/Reviews.py
@@ -1,21 +1,20 @@
 import streamlit as st
-import time
 from api.reviews import Review
 from globals import logged_user
 
 def list_reviews_page():
 
     if not logged_user or not logged_user.get("id"):
-        st.error("Você precisa estar logado para visualizar as avaliações.")
+        st.error("You must be logged in to see Reviews.")
         return
 
     review_model = Review()
     reviews = review_model.fetch_all_reviews()
 
-    st.title("Todas as Avaliações")
+    st.title("All Reviews")
 
     if not reviews:
-        st.info("Nenhuma avaliação encontrada.")
+        st.info("No reviews found.")
     else:
         for review in reviews:
             review_id = review.get("id")
@@ -26,17 +25,16 @@ def list_reviews_page():
             user_id = review.get("user_id")
 
             with st.container():
-                st.subheader(f"Restaurante: {restaurant_name}")
-                st.write(f"**Usuário:** {user_name}")
-                st.write(f"**Descrição:** {description}")
-                st.write(f"**Nota:** {grade:.1f} estrelas")
+                st.subheader(f"Restaurant: {restaurant_name}")
+                st.write(f"**User:** {user_name}")
+                st.write(f"**Description:** {description}")
+                st.write(f"**Stars:** {grade:.1f}")
 
                 if user_id == logged_user["id"]:
-                    if st.button("Excluir Avaliação", key=f"delete_{review_id}", help="Excluir esta avaliação"):
+                    if st.button("Delete Review", key=f"delete_{review_id}", help="Delete this review"):
                         if review_model.delete_review(review_id):
-                            st.success("Avaliação excluída com sucesso!")
-                            time.sleep(2)
+                            st.success("Review deleted successfully!")
                             st.rerun()
                         else:
-                            st.error("Falha ao excluir a avaliação.")
+                            st.error("Failed to delete the review.")
                 st.markdown("---")

a5776a27a5f13fd737fd4b0cfbc454c862c5a7be
Arthur de Sá Antero <88858866+ArthurAntero@users.noreply.github.com>
2025-01-22 21:15:00 -0300

    Update Profile.py

--- src/_pages/Profile.py
+++ src/_pages/Profile.py
@@ -4,19 +4,23 @@ from globals import logged_user
 
 def profile_page():
     if logged_user.get("id") is not None:
-        st.header("Minha Conta")
-        st.write(f"**Nome:** {logged_user['name']}")
+        st.header("My Account")
+        st.write(f"**Name:** {logged_user['name']}")
         st.write(f"**Email:** {logged_user['email']}")
 
-        if st.button("Sair"):
+        if st.button("Log out"):
             logged_user["id"] = None
             logged_user["name"] = None
             logged_user["email"] = None
 
-            st.success("Você saiu da sua conta.")
+            st.success("You have been logged out.")
             st.experimental_set_query_params(page="Login")
             st.rerun()
     else:
-        st.warning("Você precisa estar logado para acessar esta página.")
+        st.warning("You need to be logged in to access this page.")
         st.experimental_set_query_params(page="Login")
         st.rerun()
+
+
+import streamlit as st
+from api.users import User

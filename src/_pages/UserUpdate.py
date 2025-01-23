8d9d3025a15812c4e1b72388d3a6b50a832731f0
Arthur de Sá Antero <88858866+ArthurAntero@users.noreply.github.com>
2025-01-22 21:18:54 -0300

    Update UserUpdate.py

--- src/_pages/UserUpdate.py
+++ src/_pages/UserUpdate.py
@@ -1,16 +1,14 @@
-import time
 import streamlit as st
 from api.users import User
 from globals import logged_user
 
 def update_user_page():
     if not logged_user or not logged_user.get("id"):
-        st.error("Você precisa estar logado para atualizar seu perfil.")
+        st.error("You must be logged in to update your profile.")
         return
 
-    st.title("Atualizar Perfil de Usuário")
+    st.title("Update User Profile")
 
-    # Exibir os dados atuais do usuário
     user_model = User()
     user_model.id = logged_user["id"]
     current_email = logged_user["email"]
@@ -18,24 +16,23 @@ def update_user_page():
 
     with st.form(key="update_user_form"):
         new_email = st.text_input("Email", value=current_email)
-        new_name = st.text_input("Nome", value=current_name)
+        new_name = st.text_input("Name", value=current_name)
 
-        submit_button = st.form_submit_button("Atualizar Perfil")
+        submit_button = st.form_submit_button("Update Profile")
 
         if submit_button:
             if not new_email or not new_name:
-                st.error("Os campos de email e nome devem ser preenchidos.")
+                st.error("Both email and name fields must be filled.")
                 return
 
             user_model.email = new_email
             user_model.name = new_name
 
             if user_model.update_user():
-                st.success("Perfil atualizado com sucesso!")
-                time.sleep(1)
+                st.success("User profile updated successfully!")
                 logged_user["email"] = new_email
                 logged_user["name"] = new_name
                 st.experimental_set_query_params(page="Profile")
                 st.rerun()
             else:
-                st.error("Falha ao atualizar o perfil. Por favor, tente novamente.")
+                st.error("Failed to update user profile. Please try again.")
